832c233ff460b2639b7115a3e3cfb90a9e29156e
vollnhals <lion.vollnhals@googlemail.com>
2016-11-07 16:06:45 +0100

    Switch to datadog/statsd

--- lib/datadoge.rb
+++ lib/datadoge.rb
@@ -1,6 +1,6 @@
 require 'datadoge/version'
 require 'gem_config'
-require 'statsd'
+require 'datadog/statsd'
 
 module Datadoge
   include GemConfig::Base
@@ -12,7 +12,7 @@ module Datadoge
 
   class Railtie < Rails::Railtie
     initializer "datadoge.configure_rails_initialization" do |app|
-      $statsd = Statsd.new(Datadoge.configuration.statsd_host)
+      $statsd = Datadog::Statsd.new(Datadoge.configuration.statsd_host)
 
       ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*args|
         event = ActiveSupport::Notifications::Event.new(*args)
